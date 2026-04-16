79f06c474c6610a156823ca2ffbbb5b3868d0cfa
Krishna <krishna@Krishnas-MacBook-Air.local>
2026-04-16 15:56:17 +0530

    done

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,19 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   server: {
     port: 3011,
+    host: true,
     allowedHosts: ["hri.thefirstimpression.ai"],
   },
+
   preview: {
+    port: 3011,
+    host: true,
     allowedHosts: ["hri.thefirstimpression.ai"],
   },
+
   resolve: {
     alias: {
       "@": new URL("./src", import.meta.url).pathname,
