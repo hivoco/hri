ca8f947d288686380b135da38fefdfdf7c5d8570
Krishna <krishna@Krishnas-MacBook-Air.local>
2026-04-16 15:27:29 +0530

    port done

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3011,
-    allowedHosts: true,
-  },
-  preview: {
-    allowedHosts: true,
+    allowedHosts: ['hri.thefirstimpression.ai'],
   },
   resolve: {
     alias: {
-      "@": new URL("./src", import.meta.url).pathname,
+      '@': new URL('./src', import.meta.url).pathname,
     },
   },
-});
+})
